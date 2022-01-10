6fd94f0124c5c838cbebfaffd5ac1b579c17b241
GBL-Desktop-WSL <iglesiasg95@gmail.com>
2022-01-10 14:25:08 -0300

    change elem names, add reqWrap, user working api

--- scripts/api.js
+++ scripts/api.js
@@ -1,6 +1,32 @@
+const apiNoUsersOpt = document.querySelector("#api-no-users");
+const apiUsersOpt = document.querySelector("#api-users");
+
+const notesBtn = document.querySelector("#notes");
+const usersBtn = document.querySelector("#users");
+const usersDropdownLeyend = document.querySelector("#users-dropdown-leyend");
+const usersDropdownContainer = document.querySelector("#container-api-dropdown-users");
+const usersDropdownContent = document.querySelector("#users-dropdown-content");
+
+const getAllBtn = document.querySelector("#get-all");
+const getOneBtn = document.querySelector("#get-one");
+const postBtn = document.querySelector("#post");
+const putBtn = document.querySelector("#put");
+const delBtn = document.querySelector("#delete");
+
+const uriField = document.querySelector("#uri");
+const idField = document.querySelector("#resource-id");
+
+const apiReqTextarea = document.querySelector("#api-request");
+const apiResTextarea = document.querySelector("#api-response");
+
+let bearerToken = null;
+
+const cleanTextarea = (el) => (el.value = "");
+
+const printTextarea = (el, data) => (el.value = JSON.stringify(data, undefined, 4));
+
 const sendReq = async (args) => {
     const { method = "get", uri = "", id = "", data = {}, headers = { "Content-Type": "application/json" } } = args;
-    // data = method === "get" ? "" : data;
     try {
         const res = await axios({
             method,
@@ -15,30 +41,80 @@ const sendReq = async (args) => {
     }
 };
 
-const notesBtn = document.querySelector("#notes");
-const usersBtn = document.querySelector("#users");
+const requestWrapper = async (btn, args) => {
+    try {
+        btn.classList.add("is-success");
+        btn.classList.add("is-loading");
+        const data = await sendReq(args);
+        btn.classList.remove("is-loading");
+        btn.classList.remove("is-success");
+        cleanTextarea(apiResTextarea);
+        if (btn !== delBtn) printTextarea(apiResTextarea, data);
+    } catch (err) {
+        btn.classList.remove("is-loading");
+        btn.classList.remove("is-success");
+        cleanTextarea(apiResTextarea);
+        printTextarea(apiResTextarea, "api conection error");
+    }
+};
 
-const getAllBtn = document.querySelector("#get-all");
-const getOneBtn = document.querySelector("#get-one");
-const postBtn = document.querySelector("#post");
-const putBtn = document.querySelector("#put");
-const delBtn = document.querySelector("#delete");
+const idCheck = () => {
+    if (!idField.value) {
+        cleanTextarea(apiResTextarea);
+        printTextarea(apiResTextarea, "id field empty");
+        return false;
+    }
+    return true;
+};
 
-const uri = document.querySelector("#uri");
-uri.value = NOTES_URI;
-const id = document.querySelector("#resource-id");
-const apiReq = document.querySelector("#api-request");
-apiReq.value = JSON.stringify(EXAMPLE_NOTE_REQUEST, undefined, 4);
-const apiRes = document.querySelector("#api-response");
+const createEventsUsers = () => {
+    const aElements = document.querySelectorAll("#users-dropdown-content .dropdown-item");
+    console.log(aElements);
 
-const cleanTextarea = (el) => (el.value = "");
+    aElements.forEach((el) => {
+        el.addEventListener("click", async () => {
+            usersDropdownLeyend.innerText = el.innerText;
+            console.log(el.innerText);
+            const { token } = await sendReq({ method: "post", uri: `${BASE_URI}/login`, data: { username: el.innerText, password: el.innerText } });
+            console.log(token);
+            HEADER.Authorization = `Bearer ${token}`;
+        });
+    });
+};
 
-const printTextarea = (el, data) => (el.value = JSON.stringify(data, undefined, 4));
+addEventListener("DOMContentLoaded", () => {
+    uriField.value = NOTES_URI;
+    apiReqTextarea.value = JSON.stringify(EXAMPLE_NOTE_REQUEST, undefined, 4);
+});
+
+apiNoUsersOpt.addEventListener("click", (e) => {
+    apiNoUsersOpt.classList.add("is-active");
+    apiUsersOpt.classList.remove("is-active");
+    usersDropdownContainer.classList.add("is-hidden");
+    HEADER.Authorization = "";
+});
+
+apiUsersOpt.addEventListener("click", async (e) => {
+    apiUsersOpt.classList.add("is-active");
+    apiNoUsersOpt.classList.remove("is-active");
+    usersDropdownContainer.classList.remove("is-hidden");
+    const loginData = await sendReq({ method: "get", uri: `${BASE_URI}/users` });
+    cleanTextarea(apiResTextarea);
+    printTextarea(apiResTextarea, loginData);
+    loginData.forEach((user) => {
+        const aElement = document.createElement("a");
+        aElement.setAttribute("id", user.username);
+        aElement.classList.add("dropdown-item");
+        aElement.innerText = user.username;
+        usersDropdownContent.appendChild(aElement);
+    });
+    createEventsUsers();
+});
 
 notesBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    uri.value = NOTES_URI;
-    apiReq.value = JSON.stringify(EXAMPLE_NOTE_REQUEST, undefined, 4);
+    uriField.value = NOTES_URI;
+    apiReqTextarea.value = JSON.stringify(EXAMPLE_NOTE_REQUEST, undefined, 4);
     notesBtn.classList.add("is-success");
     notesBtn.classList.remove("is-info");
     usersBtn.classList.add("is-info");
@@ -46,13 +122,13 @@ notesBtn.addEventListener("click", async (e) => {
     getOneBtn.removeAttribute("disabled", "");
     putBtn.removeAttribute("disabled", "");
     delBtn.removeAttribute("disabled", "");
-    cleanTextarea(apiRes);
+    cleanTextarea(apiResTextarea);
 });
 
 usersBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    uri.value = USERS_URI;
-    apiReq.value = JSON.stringify(EXAMPLE_USER_REQUEST, undefined, 4);
+    uriField.value = USERS_URI;
+    apiReqTextarea.value = JSON.stringify(EXAMPLE_USER_REQUEST, undefined, 4);
     usersBtn.classList.add("is-success");
     usersBtn.classList.remove("is-info");
     notesBtn.classList.add("is-info");
@@ -60,62 +136,30 @@ usersBtn.addEventListener("click", async (e) => {
     getOneBtn.setAttribute("disabled", "");
     putBtn.setAttribute("disabled", "");
     delBtn.setAttribute("disabled", "");
-    cleanTextarea(apiRes);
+    cleanTextarea(apiResTextarea);
 });
 
 getAllBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    getAllBtn.classList.add("is-success");
-    getAllBtn.classList.add("is-loading");
-    const data = await sendReq({ method: "get", uri: uri.value });
-    getAllBtn.classList.remove("is-loading");
-    getAllBtn.classList.remove("is-success");
-    cleanTextarea(apiRes);
-    printTextarea(apiRes, data);
+    await requestWrapper(getAllBtn, { method: "get", uri: uriField.value, headers: HEADER });
 });
 
 getOneBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    getOneBtn.classList.add("is-success");
-    getOneBtn.classList.add("is-loading");
-    const data = await sendReq({ method: "get", uri: uri.value, id: id.value });
-    getOneBtn.classList.remove("is-loading");
-    getOneBtn.classList.remove("is-success");
-    cleanTextarea(apiRes);
-    printTextarea(apiRes, data);
+    if (idCheck()) await requestWrapper(getOneBtn, { method: "get", uri: uriField.value, id: idField.value, headers: HEADER });
 });
 
 postBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    postBtn.classList.add("is-success");
-    postBtn.classList.add("is-loading");
-    const data = await sendReq({ method: "post", uri: uri.value, data: JSON.parse(apiReq.value) });
-    postBtn.classList.remove("is-loading");
-    postBtn.classList.remove("is-success");
-    console.log(uri.value);
-    console.log(id.value);
-    cleanTextarea(apiRes);
-    printTextarea(apiRes, data);
+    await requestWrapper(postBtn, { method: "post", uri: uriField.value, data: JSON.parse(apiReqTextarea.value), headers: HEADER });
 });
 
 putBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    putBtn.classList.add("is-success");
-    putBtn.classList.add("is-loading");
-    const data = await sendReq({ method: "put", uri: uri.value, id: id.value, data: JSON.parse(apiReq.value) });
-    putBtn.classList.remove("is-loading");
-    putBtn.classList.remove("is-success");
-    cleanTextarea(apiRes);
-    printTextarea(apiRes, data);
+    if (idCheck()) await requestWrapper(putBtn, { method: "put", uri: uriField.value, id: idField.value, data: JSON.parse(apiReqTextarea.value), headers: HEADER });
 });
 
 delBtn.addEventListener("click", async (e) => {
     e.preventDefault();
-    delBtn.classList.add("is-success");
-    delBtn.classList.add("is-loading");
-    const data = await sendReq({ method: "delete", uri: uri.value, id: id.value });
-    delBtn.classList.remove("is-loading");
-    delBtn.classList.remove("is-success");
-    cleanTextarea(apiRes);
-    printTextarea(apiRes, data);
+    if (idCheck()) await requestWrapper(delBtn, { method: "delete", uri: uriField.value, id: idField.value, headers: HEADER });
 });
