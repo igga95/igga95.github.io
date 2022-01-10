34e0f34f2ddebfcad5cf602f28324030eb610501
GBL-Desktop-WSL <iglesiasg95@gmail.com>
2022-01-10 14:22:07 -0300

    add base_uri and header for working with users

--- utils/const.js
+++ utils/const.js
@@ -9,5 +9,11 @@ const EXAMPLE_USER_REQUEST = {
     password: "generic_password",
 };
 
+const BASE_URI = "http://localhost:3001/api";
 const NOTES_URI = "http://localhost:3001/api/notes";
 const USERS_URI = "http://localhost:3001/api/users";
+
+const HEADER = {
+    "Content-Type": "application/json",
+    Authorization: "",
+};
