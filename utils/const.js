ffe583692e8d33e8d0ecac4f3c2158bf588c6dce
Gabriel Iglesias <iglesiasg95@gmail.com>
2022-01-14 10:20:32 -0300

    change in api url

--- utils/const.js
+++ utils/const.js
@@ -8,9 +8,13 @@ const EXAMPLE_USER_POST_REQUEST = {
     password: "username",
 };
 
-const BASE_URI = "http://localhost:3001/api";
-const NOTES_URI = "http://localhost:3001/api/notes";
-const USERS_URI = "http://localhost:3001/api/users";
+const DEPLOY_URI = "https://vast-escarpment-72140.herokuapp.com";
+const BASE_URI = `${DEPLOY_URI}/api`;
+const NOTES_URI = `${DEPLOY_URI}/api/notes`;
+const USERS_URI = `${DEPLOY_URI}/api/users`;
+// const BASE_URI = "http://localhost:3001/api";
+// const NOTES_URI = "http://localhost:3001/api/notes";
+// const USERS_URI = "http://localhost:3001/api/users";
 
 const HEADER = {
     "Content-Type": "application/json",
